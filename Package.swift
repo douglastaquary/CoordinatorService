7ce13538ece2747ca8bc721de77416b24c6f0a8a
Douglas Taquary (n225862) <douglas.taquary@santander.com.br>
2020-02-09 22:42:24 -0300

    Fix spm targets dependencies.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CoordinatorService",
-            dependencies: [],
+            dependencies: ["CoordinatorServiceInterface"],
             path: "Sources"),
         .testTarget(
             name: "CoordinatorServiceTests",
