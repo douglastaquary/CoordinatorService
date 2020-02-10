05aadce900c2845672dc30c37f7c9a97949ecd84
Douglas Taquary (n225862) <douglas.taquary@santander.com.br>
2020-02-09 22:04:05 -0300

    Fix spm dependencies.

--- Package.swift
+++ Package.swift
@@ -14,14 +14,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/douglastaquary/CoordinatorServiceInterface.git",
-                  from: "0.1.0"),
+                  from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CoordinatorService",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "CoordinatorServiceTests",
             dependencies: ["CoordinatorService"]),
